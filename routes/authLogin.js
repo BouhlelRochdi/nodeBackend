15d7f1c6fb9aaa0065566aae70717ee90f21adbb
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-04-17 12:33:01 +0100

    compare crypted password and the login one

--- routes/authLogin.js
+++ routes/authLogin.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const userSchema = require('../models/userSchema');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 
-router.get('/login', async(req, res) => {
-    
-//     await userSchema.findOne({ email: req.body.email, password: req.body.password }, function (err, user) {
-//     console.log(user);
-//         if (!user){
-//         res.json({ message: '===> not registred, Please Try again'});
-//     } 
-//     else res.json({ message: '===> User exist, welcome'});
-// });
 
- 
-const user = await userSchema.findOne({ email: req.body.email, password: req.body.password }); 
-    console.log(user);
-        if (!user){
-        res.json({ message: '===> not registred, Please Try again'});
-    } 
-    else res.json({ message: '===> User exist, welcome'});
+router.get('/login', async (req, res) => {
+
+    //     await userSchema.findOne({ email: req.body.email, password: req.body.password }, function (err, user) {
+    //     console.log(user);
+    //         if (!user){
+    //         res.json({ message: '===> not registred, Please Try again'});
+    //     } 
+    //     else res.json({ message: '===> User exist, welcome'});
+    // });
+    try { // j'essaie ces instructions avant, si une erreur existe skip to catch 
+        const user = await userSchema.findOne({ email: req.body.email }); // cela nous rendre null si l'email n'existe pas
+        console.log(user);
+        if (user === null) { // si l'email n'existe pas on affiche un msg d'erreur au lieu d'obtenir une erreur au sys
+            res.json({ message: '===> mail does not exist' });
+        }
+        else { // si l'email existe on procede a comparer le password "deja enregistrer crypter"
+            const result = await bcrypt.compare(req.body.password, user.password);
+            //result contient la comparaison entre le pwd non crypter et user.password crypter/bcrypt compare decrypte et retourni true or false
+            if (result) { //si bcrypt.compare verifie et donne un true
+                res.json({ message: '===> User exist, welcome' });
+            }
+            else {
+                res.json({ message: '===> not registred, Please Try again' });
+            }
+        }
+    } catch (error) { //gestion des erreurs global
+        res.status(500).json({ message: 'internal server error' });
+    }
+
 });
 
 
