70cb32ce8327a1c6066ea4f3f737d222020b5c20
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-04-17 12:08:14 +0100

    connection with status error and success

--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,8 @@
 const mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost:27017/challenge1', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/challenge1', {useNewUrlParser: true, useUnifiedTopology: true}).then(success => {
+    console.log('successfuly connected to DB');
+}).catch(error => {
+    console.log('error to connecting to DB');
+});
 //mongoose.connect('mongodb://localhost:27017/challenge2', {useNewUrlParser: true});
 
